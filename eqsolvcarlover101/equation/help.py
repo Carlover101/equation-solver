cafc2cb454373d881a48f571540e171f67e78f2b
Zakkai Thomas <76952730+Carlover101@users.noreply.github.com>
2022-02-17 13:27:29 -0500

    Create help.py

---
+++ eqsolvcarlover101/equation/help.py
@@ -0,0 +1,5 @@
+class help():
+  def __init__(self):
+    import os
+    os.system("clear")
+    print("Type equation.commands() to get started or click on 'Code' to read the README.md file for for information.")
