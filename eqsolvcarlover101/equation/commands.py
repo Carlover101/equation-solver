11f4f7db4d0fe8ea59e8366ce673c7d5d083d465
Zakkai Thomas <76952730+Carlover101@users.noreply.github.com>
2022-02-17 13:27:16 -0500

    Create commands.py

---
+++ eqsolvcarlover101/equation/commands.py
@@ -0,0 +1,5 @@
+class commands():
+  def __init__(self):
+    import os
+    os.system("clear")
+    print("The commands are: \nequation.quadsolve() - For quadratic equations. \nequation.slopeint() - For slope-intercept equations. - NEW: May still have bugs, but works for right now :) \nequation.help() - Brings up the help page.")
