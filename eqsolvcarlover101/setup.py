1bfd0d08fb484691969d4884a08c9d218ecec044
Zakkai Thomas <76952730+Carlover101@users.noreply.github.com>
2022-02-22 12:15:23 -0500

    Update setup.py

--- eqsolvcarlover101/setup.py
+++ eqsolvcarlover101/setup.py
@@ -1,21 +1,26 @@
 import setuptools
+import os.path
 
-with open("/home/runner/Package/eqsolvcarlover101/README.md", "r") as fhandle:
-    long_description = fhandle.read()
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setuptools.setup(
     name="eqsolvcarlover101",
-    version="1.1.2",
+    version="1.2",
     author="Zakkai Thomas",
     author_email="zmanmustang2017@gmail.com",
     description="Automatic equation solver",
-    long_description=long_description,
+    long_description=read("README.md"),
     long_description_content_type="text/markdown",
     url="https://github.com/Carlover101/equation-solver",
     packages=["eqsolvcarlover101/equation","eqsolvcarlover101"],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
