9bdd1a3b304b2387d369bd1953fbdc26b364f54e
Zakkai Thomas <76952730+Carlover101@users.noreply.github.com>
2022-02-22 12:17:02 -0500

    Update help.py

--- eqsolvcarlover101/equation/help.py
+++ eqsolvcarlover101/equation/help.py
@@ -1,5 +1,8 @@
 class help():
   def __init__(self):
     import os
-    os.system("clear")
+    if os.name == "nt":
+      os.system("cls")
+    else:
+      os.system("clear")
     print("Type equation.commands() to get started or click on 'Code' to read the README.md file for for information.")
