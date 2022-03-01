8cf3cb88ec39008e8c637a8da2e952e4da1f89d0
Zakkai Thomas <76952730+Carlover101@users.noreply.github.com>
2022-03-01 12:18:20 -0500

    Update commands.py

--- eqsolvcarlover101/equation/commands.py
+++ eqsolvcarlover101/equation/commands.py
@@ -5,4 +5,4 @@ class commands():
       os.system("cls")
     else:
       os.system("clear")
-    print("The commands are: \neqsolvcarlover101.quadsolve() - For quadratic equations. \neqsolvcarlover101.slopeint() - For slope-intercept equations. \neqsolvcarlover101.midpoint() - For finding the midpoint of two points. \neqsolvcarlover101.help() - Brings up the help page.")
+    print("The commands are: \neqsolvcarlover101.quadsolve() - For quadratic equations. \neqsolvcarlover101.slopeint() - For slope-intercept equations. \neqsolvcarlover101.midpoint() - For finding the midpoint of two points. \neqsolvcarlover101.perpint() - Takes a slope and an intersection point and returns the slope-intercept equation of the perpendicular line. \neqsolvcarlover101.help() - Brings up the help page.")
